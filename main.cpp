0161bf487ef5ee9e7e6df159f8dbf56dfe2a256b
Oleg <junknewera@gmail.com>
2026-01-25 00:34:12 +0300

    v 0.4.0 change  args, improve functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <ios>
 #include <iostream>
 #include <limits>
+#include <string>
+#include <variant>
 #include <vector>
 
 struct Task {
@@ -8,53 +9,79 @@ struct Task {
   bool done = false;
 };
 
-void add(std::vector<Task> &todoList);
+int taskNumRequest();
+void add(std::vector<Task> &todoList, const std::string userInput);
 void list(const std::vector<Task> &todoList);
-void done(std::vector<Task> &todoList);
-void remove(std::vector<Task> &todoList);
+void done(std::vector<Task> &todoList, const int userInput);
+void delTask(std::vector<Task> &todoList, const int userInput);
 
 int main() {
   std::vector<Task> todoList{};
   std::string userInput;
+  std::string cmd;
+  std::string flag;
 
   std::cout << "Todo List." << std::endl;
   while (true) {
     std::cout << "Enter one of the following:\n"
-                 "add - to add any task\n"
+                 "add [task] - to add any task\n"
                  "list - to check current tasks\n"
-                 "done - to mark any task as done\n"
-                 "del - to remove any task\n"
+                 "done [num] - to mark any task as done\n"
+                 "del [num] - to remove any task\n"
                  "q - to quit\n";
+
     std::getline(std::cin, userInput);
-    if (userInput == "q") {
-      break;
-    } else if (userInput == "add") {
-      add(todoList);
-    } else if (userInput == "list") {
-      list(todoList);
-    } else if (userInput == "done") {
-      list(todoList);
-      done(todoList);
-    } else if (userInput == "del") {
-      list(todoList);
-      remove(todoList);
+    auto split = userInput.find(' ');
+    if (split != std::string::npos) {
+      cmd = userInput.substr(0, split);
+      flag = userInput.substr(split + 1, userInput.size());
     } else {
-      std::cout << "Unknown command: " << userInput << std::endl;
+      cmd = userInput;
+      flag.clear();
+    }
+
+    if (cmd == "q") {
+      break;
+    } else if (cmd == "add") {
+      add(todoList, flag);
+    } else if (cmd == "list") {
+      list(todoList);
+    } else if (cmd == "done") {
+      int intFlag;
+      if (!flag.empty()) {
+        intFlag = std::stoi(flag);
+      } else {
+        intFlag = 0;
+      }
+      done(todoList, intFlag);
+    } else if (cmd == "del") {
+      int intFlag;
+      if (!flag.empty()) {
+        intFlag = std::stoi(flag);
+      } else {
+        intFlag = 0;
+      }
+      delTask(todoList, intFlag);
+    } else {
+      std::cout << "Unknown command: " << cmd << std::endl;
     }
     std::cout << std::endl;
   }
 }
 
-void add(std::vector<Task> &todoList) {
+void add(std::vector<Task> &todoList, const std::string userInput) {
   Task task;
-
-  std::cout << "Enter task name: ";
-  std::getline(std::cin, task.text);
-  if (task.text.empty()) {
-    std::cout << "No task provided.\n";
-    return;
+  if (userInput.empty()) {
+    std::cout << "Enter task name: ";
+    std::getline(std::cin, task.text);
+    if (task.text.empty()) {
+      std::cout << "No task provided.\n";
+      return;
+    }
+    std::cout << std::endl;
+  } else {
+    task.text = userInput;
   }
-  std::cout << std::endl;
 
   todoList.push_back(task);
 }
@@ -63,35 +90,48 @@ void list(const std::vector<Task> &todoList) {
     std::cout << "Todo List is empty!\n";
   } else {
     int i = 1;
-    for (auto &task : todoList) {
+    for (const auto &task : todoList) {
       std::cout << i++ << (task.done ? " 🗹 " : " ☐ ");
       std::cout << task.text << std::endl;
     }
   }
 }
-void done(std::vector<Task> &todoList) {
+void done(std::vector<Task> &todoList, const int userInput) {
   int taskNum;
-  std::cout << "Enter task number: ";
-  std::cin >> taskNum;
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  if (!userInput) {
+    taskNum = taskNumRequest();
+  } else {
+    taskNum = userInput;
+  }
 
   if (taskNum < 1 || static_cast<size_t>(taskNum) > todoList.size()) {
     std::cout << "No such task";
     return;
   }
-  todoList[taskNum - 1].done = true;
+  todoList[userInput - 1].done = true;
+
   std::cout << std::endl;
 }
-void remove(std::vector<Task> &todoList) {
+void delTask(std::vector<Task> &todoList, const int userInput) {
   int taskNum;
-  std::cout << "Enter task number: ";
-  std::cin >> taskNum;
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
+  if (!userInput) {
+    taskNum = taskNumRequest();
+  } else {
+    taskNum = userInput;
+  }
   if (taskNum < 1 || static_cast<size_t>(taskNum) > todoList.size()) {
     std::cout << "No such task";
     return;
   }
   todoList.erase(todoList.begin() + taskNum - 1);
+
   std::cout << std::endl;
+}
+int taskNumRequest() {
+  int taskNum;
+  std::cout << "Enter task number: ";
+  std::cin >> taskNum;
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+  return taskNum;
 }
