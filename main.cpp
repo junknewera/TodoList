d2076921a9b7b2eee9a2bffce1d8aee368a6084e
Oleg <junknewera@gmail.com>
2026-01-24 22:37:00 +0300

    v 0.3.0 add del func

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ struct Task {
 void add(std::vector<Task> &todoList);
 void list(const std::vector<Task> &todoList);
 void done(std::vector<Task> &todoList);
+void remove(std::vector<Task> &todoList);
 
 int main() {
   std::vector<Task> todoList{};
@@ -18,12 +19,12 @@ int main() {
 
   std::cout << "Todo List." << std::endl;
   while (true) {
-    std::cout << "Enter one of the following: " << std::endl;
-    std::cout << "add - to add any task" << std::endl;
-    std::cout << "list - to check current tasks" << std::endl;
-    std::cout << "done - to mark any task as done" << std::endl;
-    std::cout << "q - to quit" << std::endl;
-
+    std::cout << "Enter one of the following:\n"
+                 "add - to add any task\n"
+                 "list - to check current tasks\n"
+                 "done - to mark any task as done\n"
+                 "del - to remove any task\n"
+                 "q - to quit\n";
     std::getline(std::cin, userInput);
     if (userInput == "q") {
       break;
@@ -34,6 +35,11 @@ int main() {
     } else if (userInput == "done") {
       list(todoList);
       done(todoList);
+    } else if (userInput == "del") {
+      list(todoList);
+      remove(todoList);
+    } else {
+      std::cout << "Unknown command: " << userInput << std::endl;
     }
     std::cout << std::endl;
   }
@@ -58,7 +64,7 @@ void list(const std::vector<Task> &todoList) {
   } else {
     int i = 1;
     for (auto &task : todoList) {
-      std::cout << i++ << (task.done ? ". 🗹\n" : ". ☐\n");
+      std::cout << i++ << (task.done ? " 🗹 " : " ☐ ");
       std::cout << task.text << std::endl;
     }
   }
@@ -74,5 +80,18 @@ void done(std::vector<Task> &todoList) {
     return;
   }
   todoList[taskNum - 1].done = true;
+  std::cout << std::endl;
+}
+void remove(std::vector<Task> &todoList) {
+  int taskNum;
+  std::cout << "Enter task number: ";
+  std::cin >> taskNum;
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+  if (taskNum < 1 || static_cast<size_t>(taskNum) > todoList.size()) {
+    std::cout << "No such task";
+    return;
+  }
+  todoList.erase(todoList.begin() + taskNum - 1);
   std::cout << std::endl;
 }
