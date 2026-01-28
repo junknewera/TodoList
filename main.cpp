49b3e22e9c89f65d3d491a48db9d1936bc748f15
Oleg <junknewera@gmail.com>
2026-01-28 21:52:38 +0300

    tmp commit

--- main.cpp
+++ main.cpp
@@ -7,9 +7,12 @@
 #include <iostream>
 #include <optional>
 #include <string>
+#include <sys/types.h>
 #include <vector>
 using json = nlohmann::json;
 
+enum class CustomError { Ok, InvalidNumber, NoSuchTask, ParseError, IoError };
+void printError(const CustomError &err);
 class Task {
 private:
   uint64_t id_;
@@ -26,12 +29,151 @@ public:
   bool isDone() const { return done_; }
   void markAsDone() { done_ = true; }
 };
-struct AppState {
-  std::vector<Task> tasks;
-  uint64_t next_id = 1;
+
+class TaskManager {
+private:
+  std::vector<Task> tasks_;
+  uint64_t nextId_;
+  std::string filePath_;
+
+public:
+  TaskManager(const std::string &filePath)
+      : tasks_({}), nextId_(1), filePath_(filePath) {
+    printError(load());
+  }
+
+  void add(const std::string &text) {
+    std::string taskText;
+
+    if (text.empty()) {
+      std::cout << "Enter task name: ";
+      std::getline(std::cin, taskText);
+      if (taskText.empty()) {
+        std::cout << "No task provided.\n";
+        return;
+      }
+      std::cout << std::endl;
+    } else {
+      taskText = text;
+    }
+
+    tasks_.push_back(Task(nextId_, text));
+    nextId_++;
+  }
+  void ls(const std::string &flag = "") {
+    if (tasks_.empty()) {
+      std::cout << "Todo List is empty!\n";
+      return;
+    }
+
+    std::vector<Task> tasksToShow;
+
+    if (flag == "-d" || flag == "--done") {
+      std::copy_if(tasks_.begin(), tasks_.end(),
+                   std::back_inserter(tasksToShow),
+                   [](const Task &task) { return task.isDone(); });
+    } else if (flag == "-p" || flag == "--pending") {
+      std::copy_if(tasks_.begin(), tasks_.end(),
+                   std::back_inserter(tasksToShow),
+                   [](const Task &task) { return !task.isDone(); });
+    } else {
+      tasksToShow = tasks_;
+    }
+
+    if (flag.find("-s") != std::string::npos ||
+        flag.find("--sort") != std::string::npos) {
+      if (flag.find("id") != std::string::npos) {
+        std::sort(
+            tasksToShow.begin(), tasksToShow.end(),
+            [](const Task &i, const Task &j) { return i.getId() < j.getId(); });
+      } else if (flag.find("done") != std::string::npos) {
+        std::sort(tasksToShow.begin(), tasksToShow.end(),
+                  [](const Task &i, const Task &j) {
+                    return i.isDone() > j.isDone();
+                  });
+      }
+    }
+    int i = 1;
+    for (const auto &task : tasksToShow) {
+      std::cout << i++ << " [id=" << task.getId() << "]"
+                << (task.isDone() ? " 🗹 " : " ☐ ");
+      std::cout << task.getText() << std::endl;
+    }
+  };
+  CustomError save(const std::string &path) {
+    json root;
+    root["next_id"] = nextId_;
+    root["tasks"] = json::array();
+    for (auto &task : tasks_) {
+      json t;
+      t["id"] = task.getId();
+      t["text"] = task.getText();
+      t["done"] = task.isDone();
+      root["tasks"].push_back(t);
+    }
+    std::ofstream file(path);
+    if (!file) {
+      return CustomError::IoError;
+    }
+    file << root.dump();
+    if (!file) {
+      return CustomError::IoError;
+    }
+    return CustomError::Ok;
+  }
+  CustomError remove(uint64_t id) {
+    auto index = findIndexById(id);
+    if (!index) {
+      return CustomError::NoSuchTask;
+    }
+    tasks_.erase(tasks_.begin() + *index);
+    return CustomError::Ok;
+  }
+  CustomError markDone(uint64_t id) {
+    auto index = findIndexById(id);
+    if (!index) {
+      return CustomError::NoSuchTask;
+    }
+    tasks_[*index].markAsDone();
+    return CustomError::Ok;
+  }
+
+private:
+  CustomError load() {
+    std::ifstream file(filePath_);
+    json data;
+    tasks_.clear();
+    uint64_t max = 0;
+
+    if (!file) {
+      return CustomError::Ok;
+    }
+    try {
+      file >> data;
+      data.at("next_id").get_to(nextId_);
+      for (const auto &task : data.at("tasks")) {
+        uint64_t id = task.at("id").get<uint64_t>();
+        max = std::max(max, id);
+        const std::string text = task.at("text").get<const std::string>();
+        bool done = task.at("done").get<bool>();
+        tasks_.push_back(Task(id, text, done));
+      }
+    } catch (const std::exception &e) {
+      return CustomError::ParseError;
+    }
+    nextId_ = std::max(nextId_, max + 1);
+    return CustomError::Ok;
+  }
+  std::optional<size_t> findIndexById(uint64_t id) const {
+    auto it =
+        std::find_if(tasks_.begin(), tasks_.end(),
+                     [id](const Task &task) { return task.getId() == id; });
+    if (it == tasks_.end()) {
+      return std::nullopt;
+    }
+    return std::distance(tasks_.begin(), it);
+  }
 };
-enum class EditType { del, done };
-enum class CustomError { Ok, InvalidNumber, NoSuchTask, ParseError, IoError };
 struct ResultIndex {
   CustomError code;
   size_t index = 0;
@@ -41,28 +183,16 @@ struct ResolvedId {
   std::optional<uint64_t> id;
 };
 
-ResolvedId resolveIdFromUserNumber(const AppState &state,
-                                   const std::string &flag);
 std::string trim(const std::string &userInput);
-void add(AppState &state, const std::string &userInput);
-void ls(const AppState &state, const std::string &flag = "");
-CustomError edit(AppState &state, uint64_t id, EditType type);
 ResultIndex parseIndex(const size_t listSize, const std::string &userInput);
-std::optional<size_t> findIndexById(const AppState &state, uint64_t id);
-void printError(const CustomError &err);
-CustomError saveState(const AppState &state, const std::string &path);
-CustomError loadState(AppState &state, const std::string &path);
-void applyEditAndSave(AppState &state, const std::string &flag, EditType type,
-                      const std::string &path);
 
 int main() {
   std::string userInput;
   std::string cmd;
   std::string flag;
-  AppState state;
   std::string path = "todo.json";
 
-  printError(loadState(state, path));
+  TaskManager manager = TaskManager(path);
 
   std::cout << "Todo List." << std::endl;
   while (true) {
@@ -86,12 +216,12 @@ int main() {
     if (cmd == "q") {
       break;
     } else if (cmd == "add") {
-      add(state, flag);
-      printError(saveState(state, path));
+      manager.add(flag);
+      printError(manager.save(path));
     } else if (cmd == "ls") {
-      ls(state, flag);
+      manager.ls(flag);
     } else if (cmd == "done") {
-      applyEditAndSave(state, flag, EditType::done, path);
+      manager.markDone(parseIndex(manager.tasks));
     } else if (cmd == "del") {
       applyEditAndSave(state, flag, EditType::del, path);
     } else {
@@ -101,78 +231,6 @@ int main() {
   }
 }
 
-void add(AppState &state, const std::string &userInput) {
-  std::string text;
-  uint64_t nextId = state.next_id;
-  if (userInput.empty()) {
-    std::cout << "Enter task name: ";
-    std::getline(std::cin, text);
-    if (text.empty()) {
-      std::cout << "No task provided.\n";
-      return;
-    }
-    std::cout << std::endl;
-  } else {
-    text = userInput;
-  }
-
-  state.tasks.push_back(Task(nextId, text));
-  state.next_id++;
-}
-void ls(const AppState &state, const std::string &flag) {
-  if (state.tasks.empty()) {
-    std::cout << "Todo List is empty!\n";
-    return;
-  }
-
-  std::vector<Task> tasksToShow;
-
-  if (flag == "-d" || flag == "--done") {
-    std::copy_if(state.tasks.begin(), state.tasks.end(),
-                 std::back_inserter(tasksToShow),
-                 [](const Task &task) { return task.isDone(); });
-  } else if (flag == "-p" || flag == "--pending") {
-    std::copy_if(state.tasks.begin(), state.tasks.end(),
-                 std::back_inserter(tasksToShow),
-                 [](const Task &task) { return !task.isDone(); });
-  } else {
-    tasksToShow = state.tasks;
-  }
-
-  if (flag.find("-s") != std::string::npos ||
-      flag.find("--sort") != std::string::npos) {
-    if (flag.find("id") != std::string::npos) {
-      std::sort(
-          tasksToShow.begin(), tasksToShow.end(),
-          [](const Task &i, const Task &j) { return i.getId() < j.getId(); });
-    } else if (flag.find("done") != std::string::npos) {
-      std::sort(
-          tasksToShow.begin(), tasksToShow.end(),
-          [](const Task &i, const Task &j) { return i.isDone() > j.isDone(); });
-    }
-  }
-  int i = 1;
-  for (const auto &task : tasksToShow) {
-    std::cout << i++ << " [id=" << task.getId() << "]"
-              << (task.isDone() ? " 🗹 " : " ☐ ");
-    std::cout << task.getText() << std::endl;
-  }
-}
-CustomError edit(AppState &state, uint64_t id, EditType type) {
-  auto index = findIndexById(state, id);
-  if (!index) {
-    return CustomError::NoSuchTask;
-  }
-  switch (type) {
-  case EditType::done:
-    state.tasks[*index].markAsDone();
-    break;
-  case EditType::del:
-    state.tasks.erase(state.tasks.begin() + *index);
-    break;
-  }
-  return CustomError::Ok;
-}
 ResultIndex parseIndex(const size_t listSize, const std::string &userInput) {
   ResultIndex index;
   if (userInput.empty()) {
@@ -203,29 +261,7 @@ std::string trim(const std::string &userInput) {
   const auto last = userInput.find_last_not_of(" \t");
   return userInput.substr(first, last - first + 1);
 }
-ResolvedId resolveIdFromUserNumber(const AppState &state,
-                                   const std::string &flag) {
-  std::string input = flag;
-  if (flag.empty()) {
-    std::cout << "Enter task number: ";
-    std::getline(std::cin, input);
-  }
-  input = trim(input);
 
-  ResultIndex index = parseIndex(state.tasks.size(), input);
-  if (index.code != CustomError::Ok) {
-    return {index.code, std::nullopt};
-  }
-  return {CustomError::Ok, state.tasks[index.index].getId()};
-}
-std::optional<size_t> findIndexById(const AppState &state, uint64_t id) {
-  auto it = std::find_if(state.tasks.begin(), state.tasks.end(),
-                         [id](const Task &task) { return task.getId() == id; });
-  if (it == state.tasks.end()) {
-    return std::nullopt;
-  }
-  return std::distance(state.tasks.begin(), it);
-}
 void printError(const CustomError &err) {
   switch (err) {
   case CustomError::Ok:
@@ -242,65 +278,5 @@ void printError(const CustomError &err) {
   case CustomError::IoError:
     std::cout << "Error: Input/Output Error\n";
     return;
-  }
-}
-CustomError saveState(const AppState &state, const std::string &path) {
-  json root;
-  root["next_id"] = state.next_id;
-  root["tasks"] = json::array();
-  for (auto &task : state.tasks) {
-    json t;
-    t["id"] = task.getId();
-    t["text"] = task.getText();
-    t["done"] = task.isDone();
-    root["tasks"].push_back(t);
-  }
-  std::ofstream file(path);
-  if (!file) {
-    return CustomError::IoError;
-  }
-  file << root.dump();
-  if (!file) {
-    return CustomError::IoError;
-  }
-  return CustomError::Ok;
-}
-CustomError loadState(AppState &state, const std::string &path) {
-  std::ifstream file(path);
-  json data;
-  state.tasks.clear();
-  uint64_t max = 0;
-
-  if (!file) {
-    return CustomError::Ok;
-  }
-  try {
-    file >> data;
-    data.at("next_id").get_to(state.next_id);
-    for (const auto &task : data.at("tasks")) {
-      uint64_t id = task.at("id").get<uint64_t>();
-      max = std::max(max, id);
-      const std::string text = task.at("text").get<const std::string>();
-      bool done = task.at("done").get<bool>();
-      state.tasks.push_back(Task(id, text, done));
-    }
-  } catch (const std::exception &e) {
-    return CustomError::ParseError;
-  }
-  state.next_id = std::max(state.next_id, max + 1);
-  return CustomError::Ok;
-}
-
-void applyEditAndSave(AppState &state, const std::string &flag, EditType type,
-                      const std::string &path) {
-  auto rid = resolveIdFromUserNumber(state, flag);
-  if (rid.code != CustomError::Ok) {
-    printError(rid.code);
-    return;
-  }
-  auto r = edit(state, rid.id.value(), type);
-  printError(r);
-  if (r == CustomError::Ok) {
-    printError(saveState(state, path));
   }
 }
